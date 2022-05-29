8eb822044f197d86d1761c116806be47d616a249
matheus-lincon <blatatlygod09@gmail.com>
2022-05-29 20:55:42 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,56 @@
+# Frontend Mentor - Expenses chart component solution
+
+This is a solution to the [Expenses chart component challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/expenses-chart-component-e7yJBUdjwt). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- :white_check_mark: View the bar chart and hover over the individual bars to see the correct amounts for each day
+- :white_check_mark: See the current day’s bar highlighted in a different colour to the other bars
+- :white_check_mark: View the optimal layout for the content depending on their device’s screen size
+- :white_check_mark: See hover states for all interactive elements on the page
+- :white_check_mark: **Bonus**: Use the JSON data file provided to dynamically size the bars on the chart
+
+### Screenshot
+
+<img src="./screenshots/mobile.png" width="375px">
+<img src="./screenshots/desktop.png" min-width="375px">
+
+### Links
+
+- Live Site URL: [Expense Charts Component](https://www.matheus-lincon.github.io/expenses-chart-component)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- JavaScript
+  - Fetch
+  - Date
+  - NumberFormat (currency)
+  - eventListeners
+- CSS
+  - Flexbox
+  - CSS Grid
+  - Position absolute & relative
+- Mobile-first workflow
+
+## Author
+
+- GitHub - [matheus-lincon](https://www.github.com/matheus-lincon)
+- Frontend Mentor - [@matheus-lincon](https://www.frontendmentor.io/profile/matheus-lincon)
+- LinkedIn - [matheus.lincon.10010](https://www.linkedin.com/in/matheus-lincon-10010)
---
+++ javascript/main.js
@@ -0,0 +1,93 @@
+/* CONSUME JSON FILE */
+
+fetch('../json/data.json')
+  .then((response) => {
+    return response.json()
+  })
+  .then((jsondata) => {
+    let week = getTodayBar()
+    createBars(jsondata, week)
+
+    /* SHOW AMOUNT WHEN HOVER */
+    document.querySelectorAll('.bar').forEach((bar, index) => {
+      bar.addEventListener('mouseover', () => {
+        showAmount(jsondata, index)
+      })
+    })
+
+    document.querySelectorAll('.bar').forEach((bar, index) => {
+      bar.addEventListener('mouseout', () => {
+        hideAmount(index)
+      })
+    })
+  })
+
+/* Show amount */
+function showAmount(data, index) {
+  const priceTag = createPriceTag(data, index)
+  document.querySelectorAll('.bar')[index].appendChild(priceTag)
+}
+
+/* Hide amount */
+function hideAmount(index) {
+  const price = document.querySelector('.price')
+  document.querySelectorAll('.bar')[index].removeChild(price)
+}
+
+/* create price element */
+
+function createPriceTag(price, index) {
+  const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  })
+
+  const div = document.createElement('div')
+  div.classList.add('price')
+  div.innerText = formatter.format(price[index].amount)
+
+  return div
+}
+
+/* show today's bar */
+
+function getTodayBar() {
+  let date = new Date()
+  let todayWeek = ''
+  if (date.getDay() == 0) {
+    todayWeek = 'sun'
+  } else if (date.getDay() == 1) {
+    todayWeek = 'mon'
+  } else if (date.getDay() == 2) {
+    todayWeek = 'tue'
+  } else if (date.getDay() == 3) {
+    todayWeek = 'wed'
+  } else if (date.getDay() == 4) {
+    todayWeek = 'thu'
+  } else if (date.getDay() == 5) {
+    todayWeek = 'fri'
+  } else if (date.getDay() == 6) {
+    todayWeek = 'sat'
+  }
+  return todayWeek
+}
+
+/* DEFINE BARS HEIGHT */
+function createBars(data, week) {
+  const bars = document.querySelectorAll('.bar')
+  const dataHeight = document.querySelector('.data')
+
+  bars.forEach((bar, index) => {
+    let barHeight = data[index].amount * 2
+    if (barHeight >= dataHeight.clientHeight)
+      barHeight = dataHeight.clientHeight
+
+    let rest = dataHeight.clientHeight - barHeight
+    bar.style.height = `${barHeight}px`
+    bar.style.marginTop = `${rest}px`
+
+    if (data[index].day == week) {
+      bar.style.backgroundColor = 'var(--cyan)'
+    }
+  })
+}
